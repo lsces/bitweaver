15d81f343f568cad495f330f5152093d12d7df28
lsces <lester@lsces.co.uk>
2025-08-28 20:25:33 +0100

    Information to tidy the display of the config package in the installer

---
+++ config/admin/schema_inc.php
@@ -0,0 +1,7 @@
+<?php
+global $gBitInstaller;
+
+$gBitInstaller->registerPackageInfo( CONFIG_PKG_NAME, [
+	'description' => "Configuration package contains site specific configuration that can override the core system templates and functionality. Themes are stored in this package which can be selected between.",
+	'license' => '<a href="http://www.gnu.org/licenses/licenses.html#LGPL">LGPL</a>',
+] );
