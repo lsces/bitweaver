7a24d42ab43b6d189877e9c8d6ea5911d35001d7
lsces <lester@lsces.co.uk>
2025-09-04 11:22:35 +0100

    Tidy loose ends on master view of project. colourstrap files needs moving to submodule

--- config/includes/bit_setup_inc.php
+++ config/includes/bit_setup_inc.php
@@ -1,12 +1,12 @@
 <?php
 global $gBitSystem;
 
-$registerHash = array(
+$pRegisterHash = [
 	'package_name' => 'config',
 	'package_path' => dirname( dirname( __FILE__ ) ).'/',
-	'required_package'=> TRUE,
-);
-$gBitSystem->registerPackage( $registerHash );
+	'required_package'=> true,
+];
+$gBitSystem->registerPackage( $pRegisterHash );
 
 define( 'THEMES_PATH', CONFIG_PKG_PATH.'themes/' );
 define( 'THEMES_URL', CONFIG_PKG_URL.'themes/' );
@@ -16,4 +16,4 @@ define( 'ICONSETS_PATH', CONFIG_PKG_PATH.'iconsets/' );
 define( 'ICONSETS_URL', CONFIG_PKG_URL.'iconsets/' );
 define( 'ICONSETS_URI', CONFIG_PKG_URI.'iconsets/' );
 
-set_error_handler( 'bit_error_handler' );
+set_error_handler( '\Bitweaver\bit_error_handler' );
