3db31967a73d3855a95c23f9dd8da41fbeb0cb3d
lsces <lester@lsces.co.uk>
2026-04-05 11:15:29 +0100

    Can't use defined name until it's defined :)

--- config/admin/schema_inc.php
+++ config/admin/schema_inc.php
@@ -1,7 +1,7 @@
 <?php
 global $gBitInstaller;
 
-$gBitInstaller->registerPackageInfo( CONFIG_PKG_NAME, [
+$gBitInstaller->registerPackageInfo( 'config', [
 	'description' => "Configuration package contains site specific configuration that can override the core system templates and functionality. Themes are stored in this package which can be selected between.",
 	'license' => '<a href="http://www.gnu.org/licenses/licenses.html#LGPL">LGPL</a>',
 ] );
