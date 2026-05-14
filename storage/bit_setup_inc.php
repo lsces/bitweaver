bd828ddb0a7f9a4fc88a4f6ad747384c0570dc0a
Lester Caine <lester@lsces.co.uk>
2026-05-14 10:02:11 +0100

    php-cs-fixer tidies to php8.5 standards

--- storage/bit_setup_inc.php
+++ storage/bit_setup_inc.php
@@ -1,10 +1,10 @@
 <?php
 global $gBitSystem;
 
-$pRegisterHash = array(
+$pRegisterHash = [
 	'package_name' => 'storage',
 	'package_path' => dirname( __FILE__ ).'/',
 	'required_package'=> true,
-);
+];
 $gBitSystem->registerPackage( $pRegisterHash );
 ?>
